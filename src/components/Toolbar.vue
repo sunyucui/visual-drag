8a19fb78b693068c6d83da16021fcd923e474317
sunyucui <sunyucuiya@163.com>
2021-12-21 10:30:40 +0800

    fit(Toolbar)

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -23,7 +23,6 @@ import Preview from '@/components/Editor/Preview'
 import ExportCode from '@/components/Editor/ExportCode'
 import { commonStyle, commonAttr } from '@/custom-component/component-list'
 import eventBus from '@/utils/eventBus'
-import { deepCopy } from '@/utils/utils'
 
 export default {
     components: { Preview, ExportCode },
@@ -53,7 +52,6 @@ export default {
     created() {
         eventBus.$on('preview', this.preview)
         eventBus.$on('save', this.save)
-        eventBus.$on('clearCanvas', this.clearCanvas)
         eventBus.$on('exportCode', this.exportCode)
 
         this.scale = this.canvasStyleData.scale
@@ -63,108 +61,6 @@ export default {
             const scale = this.scale
             return value * parseInt(scale) / 100
         },
-
-        getOriginStyle(value) {
-            const scale = this.canvasStyleData.scale
-            const result = value / (parseInt(scale) / 100)
-            return result
-        },
-
-        handleScaleChange() {
-            clearTimeout(this.timer)
-            this.timer = setTimeout(() => {
-                // 画布比例设一个最小值，不能为 0
-                // eslint-disable-next-line no-bitwise
-                this.scale = (~~this.scale) || 1
-                const componentData = deepCopy(this.componentData)
-                componentData.forEach(component => {
-                    Object.keys(component.style).forEach(key => {
-                        if (this.needToChange.includes(key)) {
-                            // 根据原来的比例获取样式原来的尺寸
-                            // 再用原来的尺寸 * 现在的比例得出新的尺寸
-                            // 不能用 Math.round 防止 1 px 的边框变 0
-                            component.style[key] = Math.ceil(this.format(this.getOriginStyle(component.style[key])))
-                        }
-                    })
-                })
-
-                this.$store.commit('setComponentData', componentData)
-                this.$store.commit('setCanvasStyle', {
-                    ...this.canvasStyleData,
-                    scale: this.scale,
-                })
-            }, 1000)
-        },
-
-        lock() {
-            this.$store.commit('lock')
-        },
-
-        unlock() {
-            this.$store.commit('unlock')
-        },
-
-        compose() {
-            this.$store.commit('compose')
-            this.$store.commit('recordSnapshot')
-        },
-
-        decompose() {
-            this.$store.commit('decompose')
-            this.$store.commit('recordSnapshot')
-        },
-
-        undo() {
-            this.$store.commit('undo')
-        },
-
-        redo() {
-            this.$store.commit('redo')
-        },
-
-        handleFileChange(e) {
-            const file = e.target.files[0]
-            if (!file.type.includes('image')) {
-                toast('只能插入图片')
-                return
-            }
-
-            const reader = new FileReader()
-            reader.onload = (res) => {
-                const fileResult = res.target.result
-                const img = new Image()
-                img.onload = () => {
-                    this.$store.commit('addComponent', {
-                        component: {
-                            ...commonAttr,
-                            id: generateID(),
-                            component: 'Picture',
-                            label: '图片',
-                            icon: '',
-                            propValue: fileResult,
-                            style: {
-                                ...commonStyle,
-                                top: 0,
-                                left: 0,
-                                width: img.width,
-                                height: img.height,
-                            },
-                        },
-                    })
-
-                    this.$store.commit('recordSnapshot')
-
-                    // 修复重复上传同一文件，@change 不触发的问题
-                    document.querySelector('#input').setAttribute('type', 'text')
-                    document.querySelector('#input').setAttribute('type', 'file')
-                }
-
-                img.src = fileResult
-            }
-
-            reader.readAsDataURL(file)
-        },
-
         preview() {
             this.isShowPreview = true
             this.$store.commit('setEditMode', 'preview')
@@ -179,12 +75,6 @@ export default {
             localStorage.setItem('componentsData', JSON.stringify(this.componentData))
             localStorage.setItem('componentsStyle', JSON.stringify(this.canvasStyleData))
             this.$message.success('保存成功')
-        },
-
-        clearCanvas() {
-            this.$store.commit('setCurComponent', { component: null, index: null })
-            this.$store.commit('setComponentData', [])
-            this.$store.commit('recordSnapshot')
         },
 
         handlePreviewChange() {
